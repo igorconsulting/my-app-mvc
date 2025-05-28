7ff1f843930fe50284665ebe69e751b751805580
Igor-personal <icaetanodiniz@gmail.com>
2025-05-28 08:42:20 -0300

    add run

---
+++ run.py
@@ -0,0 +1,15 @@
+from src.main.server.server import app
+
+if __name__ == "__main__":
+    app.run(port=3001)
+
+
+'''
+{
+    "usuario": {
+        "nome": "Henrique",
+        "idade": 32,
+        "altura": 1.76
+    }
+}
+'''
